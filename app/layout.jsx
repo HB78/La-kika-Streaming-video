e7ed18098f3dff70fadb825cd8d821d66a18fa50
HB78 <78313137+HB78@users.noreply.github.com>
2024-02-14 23:30:16 +0100

    premier prisma generate en vu du deploiement

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,24 +2,24 @@ import "@uploadthing/react/styles.css";
 import Provider from "./../context/Provider";
 import "./globals.css";
 
-// export const metadata = {
-//   metadataBase: new URL(`${process.env.NEXTAUTH_URL}`),
-//   title:
-//     "La kika est le premier site de streaming en ligne sans pub et sans inscription",
-//   description:
-//     "Regardez vos films et séries préférés en ligne sans pub et sans inscription, le site est totalement gratuit et sans pub. Ne payez plus pour regarder vos films et séries, profitez sans limite de notre catalogue de films et séries en streaming.",
-//   icons: [{ rel: "icon", url: Favicon.src }],
-//   openGraph: {
-//     title:
-//       "la kika - Le premier site de streaming sans pub et sans inscription",
-//     description:
-//       "Regardez vos films et séries préférés en ligne sans pub et sans inscription, le site est totalement gratuit et sans pub. Ne payez plus pour regarder vos films et séries, profitez sans limite de notre catalogue de films et séries en streaming.",
-//     // url: 'https://nextjs.org',
-//     // siteName: 'Next.js',
-//     locale: "en_FR",
-//     type: "website",
-//   },
-// };
+export const metadata = {
+  metadataBase: new URL(`${process.env.NEXTAUTH_URL}`),
+  title:
+    "La kika est le premier site de streaming en ligne sans pub et sans inscription",
+  description:
+    "Regardez vos films et séries préférés en ligne sans pub et sans inscription, le site est totalement gratuit et sans pub. Ne payez plus pour regarder vos films et séries, profitez sans limite de notre catalogue de films et séries en streaming.",
+  icons: [{ rel: "icon", url: Favicon.src }],
+  // openGraph: {
+  //   title:
+  //     "la kika - Le premier site de streaming sans pub et sans inscription",
+  //   description:
+  //     "Regardez vos films et séries préférés en ligne sans pub et sans inscription, le site est totalement gratuit et sans pub. Ne payez plus pour regarder vos films et séries, profitez sans limite de notre catalogue de films et séries en streaming.",
+  //   // url: 'https://nextjs.org',
+  //   // siteName: 'Next.js',
+  //   locale: "en_FR",
+  //   type: "website",
+  // },
+};
 
 export default function RootLayout({ children }) {
   return (
