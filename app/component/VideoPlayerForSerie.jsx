683f4d67b272cf81bb89f257dfb8c8c8f22eeac5
HB78 <78313137+HB78@users.noreply.github.com>
2024-04-02 19:00:09 +0200

    j'ai remis la width de videoplayerserie a 100 pourcent

--- app/component/VideoPlayerForSerie.jsx
+++ app/component/VideoPlayerForSerie.jsx
@@ -29,7 +29,7 @@ const VideoPlayerForSerie = ({ episodeOne }) => {
     <>
       {client ? (
         <ReactPlayer
-          width={"96%"}
+          width={"100%"}
           url={url}
           controls={true}
           // light is useful in case of dark mode
