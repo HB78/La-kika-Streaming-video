690c1c2c6dc4715ee7bce751e443b84ea78b9272
HB78 <78313137+HB78@users.noreply.github.com>
2024-05-01 00:09:12 +0200

    resend ajout du process env dans ladrese api

--- app/component/forms/CreateFormForgotPassword.jsx
+++ app/component/forms/CreateFormForgotPassword.jsx
@@ -23,7 +23,7 @@ const CreateFormForgotPassword = () => {
   });
 
   const onSubmitForm = async (data) => {
-    const res = await fetch(`https://lakika.vercel.app/api/forgotPassword`, {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/forgotPassword`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
