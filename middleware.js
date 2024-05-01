ff90243c43c89a0b1765670a9f57b1544363bb27
HB78 <78313137+HB78@users.noreply.github.com>
2024-05-01 20:09:09 +0200

    mise a jour du fichier middleware

--- middleware.js
+++ middleware.js
@@ -1,19 +1,15 @@
 import { NextResponse } from "next/server";
 
-const allowedOrigins = ["http://localhost:3000", "https://lakika.vercel.app"];
-
-export function middleware(req) {
+export function middleware() {
   // retrieve the current response
   const res = NextResponse.next();
 
-  req.headers.get("origin");
-
-  if (allowedOrigins.includes(origin)) {
-    res.headers.append("Access-Control-Allow-Origin", origin);
-  }
   // add the CORS headers to the response
   res.headers.append("Access-Control-Allow-Credentials", "true");
-
+  res.headers.append(
+    "Access-Control-Allow-Origin",
+    "https://lakika.vercel.app"
+  ); // replace this your actual origin
   res.headers.append(
     "Access-Control-Allow-Methods",
     "GET,DELETE,PATCH,POST,PUT"
