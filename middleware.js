4174118ea81241c25369983f30aed3ed01d236ad
HB78 <78313137+HB78@users.noreply.github.com>
2024-05-01 19:56:02 +0200

    je refais le build car nextjs n'aime pas les server actions lors du mode prod

--- middleware.js
+++ middleware.js
@@ -1,15 +1,19 @@
 import { NextResponse } from "next/server";
 
-export function middleware() {
+const allowedOrigins = ["http://localhost:3000", "https://lakika.vercel.app"];
+
+export function middleware(req) {
   // retrieve the current response
   const res = NextResponse.next();
 
+  req.headers.get("origin");
+
+  if (allowedOrigins.includes(origin)) {
+    res.headers.append("Access-Control-Allow-Origin", origin);
+  }
   // add the CORS headers to the response
   res.headers.append("Access-Control-Allow-Credentials", "true");
-  res.headers.append(
-    "Access-Control-Allow-Origin",
-    "https://lakika.vercel.app"
-  ); // replace this your actual origin
+
   res.headers.append(
     "Access-Control-Allow-Methods",
     "GET,DELETE,PATCH,POST,PUT"
