eee7831676c8f1e9fc28d81f911471d4c88a2945
HB78 <78313137+HB78@users.noreply.github.com>
2024-07-04 19:36:41 +0200

    ajout de usafe inline

--- middleware.js
+++ middleware.js
@@ -4,7 +4,7 @@ export function middleware(request) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://lakika.vercel.app;
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://lakika.vercel.app 'unsafe-inline';
     style-src 'self' 'nonce-${nonce}' https://lakika.vercel.app;
     img-src 'self' blob: data: https://lakika.vercel.app;
     font-src 'self' https://lakika.vercel.app;
