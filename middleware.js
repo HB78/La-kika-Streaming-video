0915d752977a17cb0cfe0ef0d545b8691f1e7a53
HB78 <78313137+HB78@users.noreply.github.com>
2024-07-04 19:26:18 +0200

    mise en place du middleware csp avec la doc, je teste egalement le img-sr avec le nom de mon site

--- middleware.js
+++ middleware.js
@@ -1,69 +1,40 @@
 import { NextResponse } from "next/server";
 
-const allow_origin_lists = ["https://lakika.vercel.app"];
-const isDevelopment = process.env.NODE_ENV === "development";
-
-export const middleware = async (req) => {
-  const origin = req.headers.get("origin");
+export function middleware(request) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://lakika.vercel.app;
+    style-src 'self' 'nonce-${nonce}' https://lakika.vercel.app;
+    img-src 'self' blob: data: https://lakika.vercel.app;
+    font-src 'self' https://lakika.vercel.app;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
 
-  if (origin && !allow_origin_lists.includes(origin)) {
-    return new NextResponse(null, {
-      status: 403,
-      statusText: "Forbidden",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  }
-
-  const requestHeaders = new Headers(req.headers);
+  const requestHeaders = new Headers(request.headers);
   requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  );
 
-  const cspHeader = generateCspHeader(nonce);
-  requestHeaders.set("Content-Security-Policy", cspHeader);
-
-  const res = NextResponse.next({
+  const response = NextResponse.next({
     request: {
       headers: requestHeaders,
     },
   });
-
-  // Ajouter les headers CORS
-  appendCorsHeaders(res.headers, origin);
-
-  // Ajouter les headers CSP
-  res.headers.set("Content-Security-Policy", cspHeader);
-
-  return res;
-};
-
-function appendCorsHeaders(headers, origin) {
-  headers.set("Access-Control-Allow-Credentials", "true");
-  headers.set("Access-Control-Allow-Origin", origin);
-  headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  headers.set(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Content-Type, Content-Length, Content-MDS, Accept, Accept-Version, Date, X-Api-Version"
+  response.headers.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
   );
-}
 
-function generateCspHeader(nonce) {
-  const policy = isDevelopment
-    ? `default-src 'self'; 
-       script-src 'self' 'nonce-${nonce}' 'strict-dynamic'; 
-       style-src 'self' 'nonce-${nonce}'; 
-       img-src 'self' data: https:; 
-       font-src 'self'; 
-       connect-src 'self' https:; 
-       frame-src 'self';`
-    : `default-src 'self'; 
-       script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://lakika.vercel.app; 
-       style-src 'self' 'nonce-${nonce}' https://lakika.vercel.app; 
-       img-src 'self' data: https:; 
-       font-src 'self' https://lakika.vercel.app; 
-       connect-src 'self' https:; 
-       frame-src 'self';`;
-
-  return policy.replace(/\s+/g, " ").trim();
+  return response;
 }
