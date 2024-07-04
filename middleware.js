6188b1cdb297253645d9c28588d0ea2736f7bfff
HB78 <78313137+HB78@users.noreply.github.com>
2024-07-04 19:56:41 +0200

    ajout de media src dans le csp

--- middleware.js
+++ middleware.js
@@ -3,14 +3,13 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
-    default-src 'self' style-src 'unsafe-inline' https://lakika.vercel.app;
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https://lakika.vercel.app 'unsafe-inline';
-    style-src 'self' 'nonce-${nonce}' https://lakika.vercel.app;
-    img-src 'self' blob: data: https://lakika.vercel.app;
-    font-src 'self' https://lakika.vercel.app;
+     default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
     object-src 'none';
     base-uri 'self';
-    media-src 'self' 'nonce-${nonce}' https://lakika.vercel.app;
     form-action 'self';
     frame-ancestors 'none';
     upgrade-insecure-requests;
