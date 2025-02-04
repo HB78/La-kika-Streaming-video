ba32783109c1aa22646e5f9017c0353abdc29c6c
HB78 <78313137+HB78@users.noreply.github.com>
2025-02-04 15:45:24 +0100

    j'ai ajouté un svg en forme de K dans le loading de la page contact

--- app/contact/loading.jsx
+++ app/contact/loading.jsx
@@ -1,7 +1,10 @@
+import NetflixKLetter from "../component/svg/NetflixKLetter";
+
 const Loading = () => {
   return (
-    <div className="w-full h-[100vh] bg-black absolute flex justify-center items-center text-3xl text-red-800 font-bolder">
-      Loading{" "}
+    <div className="w-full h-[100vh] bg-black absolute flex justify-center items-center text-3xl text-red-800 font-bolder gap-6">
+      <NetflixKLetter />
+      <p>Loading </p>
     </div>
   );
 };
