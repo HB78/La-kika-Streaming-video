a7bfd981d390a2ae17ca49ab20468d0a729eb676
unknown <boubttahicham@gmail.com>
2025-06-19 20:04:37 +0200

    j'ai mis uploadsmallfile pour le upload des gros fichiers

--- app/api/url/route.js
+++ app/api/url/route.js
@@ -4,6 +4,7 @@
 // Cette route sert uniquement à obtenir une URL signée de Pinata pour les uploads directs. Elle est utilisée pour les petits fichiers. UN UPLOAD EXPRESS RAPIDE ET DIRECT
 
 import { pinata } from "@/app/utils/config";
+import { isAdmin } from "@/lib/dryApiFunction/isAdmin";
 import { NextResponse } from "next/server";
 
 export const bodyParser = false;
@@ -12,6 +13,12 @@ export const dynamic = "force-dynamic";
 
 export async function GET() {
   // If you're going to use auth you'll want to verify here
+  const adminCheck = await isAdmin();
+
+  // Vérifier si c'est une erreur
+  if (adminCheck.status !== 200) {
+    return adminCheck; // Retourner directement l'erreur
+  }
   try {
     const url = await pinata.upload.public.createSignedURL({
       expires: 1000 * 60 * 60 * 24 * 30, // The only required param
@@ -19,7 +26,7 @@ export async function GET() {
     console.log("url:", url);
     return NextResponse.json({ url: url }, { status: 200 }); // Returns the signed upload URL
   } catch (error) {
-    console.log(error);
+    console.log("error: API URL", error);
     return NextResponse.json(
       { text: "Error creating API Key:" },
       { status: 500 }
